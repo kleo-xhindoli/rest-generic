e2b0b71b0a375653ef32c3b4ab6795f6bb2c3626
kleo-xhindoli <kleo.xhindoli@gmail.com>
2017-10-25 22:31:23 +0200

    fix: serve from dist

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors());
 app.use(passport.initialize());
 
 
-app.use('/admin', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public/admin/dist')));
 
 app.use('/api/', routes);
 app.use('/api/users', users);
